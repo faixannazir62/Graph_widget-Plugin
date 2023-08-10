f43af7ad143e5845e1d11c2d171bf271f3aca0da
faixannazir62 <faizanbhat46@gmail.com>
2023-08-10 23:11:09 +0530

    Space removed

--- graph-widget.php
+++ graph-widget.php
@@ -10,7 +10,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       graph-widget
 *  Domain Path:       /languages/
- */
+*/
 
  // No Direct Access
 if( !defined( 'ABSPATH' ) ) exit();
